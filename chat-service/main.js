58d8386deaeb81cfad7461439f9db1ac89bf749c
v24Zer0 <vinbesse@gmail.com>
2022-11-29 11:27:34 +0200

    Setup NextJS for frontend

--- chat-service/main.js
+++ chat-service/main.js
@@ -4,4 +4,13 @@ server.on("connection", (socket) => {
     console.log(`User ${socket.id} connected`)
 
     socket.emit("hello", "world");
+
+    socket.on("send", (data) => {
+        // data.message
+        // data.room
+        // data.createdBy
+        // data.createdAt
+
+        // socket.to(data.room).emit("receive", (data))
+    })
 })
